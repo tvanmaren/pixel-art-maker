a7ff2d015b10befeddc8de29c8c42a3181f8e4f1
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-10-14 11:41:52 -0600

    add generateCanvas & pseudo-code

---
+++ generateCanvas.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function makeDiv (class, id, listen) {
+  // inject
+  // add classification (class variable)
+  // add id
+  // do I listen?
+}
+
+function makeRow (length, class, y-id, listen) {
+  // make the divs (class, id='x# y#', listen)
+  // make a td
+  // set Parent to td
+  // make a tr
+  // set Parent to tr
+  // do I listen?
+}
+
+function makeTable (x-size, y-size, listen) {
+  // LOOP with y-size
+  //   makeRow(x-size, class=pixel, y#, listen)
+  //   make a <table>
+  //   set Parent to <table>
+}
