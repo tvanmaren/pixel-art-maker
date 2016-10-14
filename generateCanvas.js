30228f1d395b6e8a46ea8caf15e9d5b4e3cda708
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-10-14 15:25:02 -0600

    generateCanvas functional--missing listen functionality

--- generateCanvas.js
+++ generateCanvas.js
@@ -1,24 +1,53 @@
 'use strict';
 
-function makeDiv (class, id, listen) {
-  // inject
-  // add classification (class variable)
-  // add id
-  // do I listen?
+function makeCell(className, idName, listen = false) {
+    // make a Data Cell
+    var newCell = document.createElement('td');
+    // make a Div
+    var newDiv = document.createElement('div');
+    // add classification (class variable)
+    newDiv.className = className;
+    // add id
+    newDiv.id = idName;
+    // set Div's Parent to the Cell
+    newCell.appendChild(newDiv);
+    // do I listen?
+    // add listening functionality here
+    return newCell;
 }
 
-function makeRow (length, class, y-id, listen) {
-  // make the divs (class, id='x# y#', listen)
-  // make a td
-  // set Parent to td
-  // make a tr
-  // set Parent to tr
-  // do I listen?
+function makeRow(className, length, yid, listen = false) {
+    // make a Row
+    var newRow = document.createElement('tr');
+    // make the divs (class, id='x# y#', listen)
+    var newCell, xid, xyvalue;
+    for (var i = 0; i <= length; i++) {
+        xid = i.toString();
+        xyvalue = xid + ' ' + yid.toString();
+        newCell = makeCell(className, xyvalue, listen);
+        // set Cell's Parent to the Row
+        newRow.appendChild(newCell);
+    }
+    // do I listen?
+    // add listening functionality here
+    return newRow;
 }
 
-function makeTable (x-size, y-size, listen) {
-  // LOOP with y-size
-  //   makeRow(x-size, class=pixel, y#, listen)
-  //   make a <table>
-  //   set Parent to <table>
+function makeTable(xsize, ysize, listen = false) {
+    var body = document.getElementsByTagName('body');
+    // make a <table>
+    var newTable = document.createElement('table');
+    var className = 'pixel';
+    var newRow;
+    // LOOP with y-size
+    for (var i = ysize; i >= 0; i--) {
+        // makeRow(x-size, class=pixel, y#, listen)
+        newRow = makeRow(className, xsize, i, listen);
+        // set Parent to <table>
+        newTable.appendChild(newRow);
+    }
+    body[0].appendChild(newTable);
+    return newTable;
 }
+
+makeTable(20, 20);
